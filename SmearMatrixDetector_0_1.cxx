ee8c9ec44852db1586924421e7d1f833ec8baab0
Kolja Kauder <kkauder@gmail.com>
2020-06-16 15:16:23 -0400

    Should completely comply with Matrix v 0.1

--- SmearMatrixDetector_0_1.cxx
+++ SmearMatrixDetector_0_1.cxx
@@ -1,27 +1,24 @@
 // Based on
 // https://physdiv.jlab.org/DetectorMatrix/
-// From June 8 2020 XXX
+// From June 16 2020
 
+// W.r.t. to the [Detector Requirements and R&D Handbook](http://www.eicug.org/web/sites/default/files/EIC_HANDBOOK_v1.2.pdf) there have been three changes. 
 
+// For the Backward Detector the Tracking Resolution column was updated as follows:
+// eta=-3.5 - -2.5: sigma_p/p ~ 0.1%×p+2.0%   ->   sigma_p/p ~ 0.1%×p &oplus;  0.5%
+// eta=-2.5 - -2.0: sigma_p/p ~ 0.1%×p+1.0%   ->   sigma_p/p ~ 0.1%×p &oplus;  0.5%
+// eta=-2.0 - -1.0: sigma_p/p ~ 0.1%×p+1.0%   ->   sigma_p/p ~ 0.05%×p &oplus;  0.5% 
+// The abstract, reference files, and note sections of these fields have been updated accordingly. 
 
-
-// XXX Additional details from discussions within the calorimetry working group:
-// 
-// > These are two configuration for hadron endcap we talked today to consider.
-// > Base configuration:
-// > EM - W/ScFi, granularity 2.5 cm x 2.5 cm, 12% stochastic, 2% constant terms
-// > HCal- Fe/Sc, granularity 10 x 10 cm, 50% stochastic, 10% constant
-
-// > Configuration 2:
-// > EM is the same as above.
-// > HCal -Pb/Sc, 10 x 10 cm, 45% stochastic, 6% constant terms.
-
-// > Alternative ECAL
-// > Shashlyk, Pb/Sc 5.5 x5.5, 8% stochastic, 2% constant.
-
-// > Thanks,
-// > Oleg [Tsai]
-
+// Important Notes:
+// - The implementation of the original handbook matrix in SmearHandBook_1_2.cxx
+//   took some liberties and added constant terms and other specifications where
+//   they were/are missing in the matrix.
+//   The implementation here explicitly does not do so, and aims to be completely faithful
+//   to the configuration available online.
+// - Where ranges are given, the more conservative number is chosen.
+// - Without available specifications, angular resolution is assumed to be perfect.
+// - 
 
 #include "eicsmear/erhic/VirtualParticle.h"
 #include "eicsmear/smear/Acceptance.h"
@@ -65,7 +62,6 @@ Smear::Detector BuildMatrixDetector_0_1() {
   // the calculation of smeared kinematics. Sophisticated calculations are best redone in user code
   // from the smeared particles directly.
   det.SetEventKinematicsCalculator("NM DA JB");
-
     
   // IMPORTANT: There are two traps (will be addressed in future releases):
   //            1) If you smear E but don't provide phi, theta smearers, those values will be
@@ -74,40 +70,29 @@ Smear::Detector BuildMatrixDetector_0_1() {
   //               by a consistency enforcer in Detector::Smear()
 
 
-  // As detailed above, we always need phi and theta smearers.
-  // In the absence of handbook values, assume 0.001 radians throughout the acceptance
-  // That's a reasonably conservative guesstimate for tracking,
-  // and probably a bit too good for most calorimeters.
-  // In reality, you would have granularity and clustering assumptions
-  // which are best handled in an afterburner (like tracking efficiency).
 
-  // Phi and theta smearing for all particles
-  // ----------------------------------------
+  // Perfect phi and theta for all particles
+  // ---------------------------------------
   // total coverage of the handbook for tracker and hcal is -3.5 < eta < 3.5
-
-  // XXX angular resolution?
   Smear::Acceptance::Zone AngleZoneHadronic(ThetaFromEta ( 3.5 ),ThetaFromEta ( -3.5 ));
-  Smear::Device SmearThetaHadronic(Smear::kTheta, "0.001");
+  Smear::Device SmearThetaHadronic(Smear::kTheta, "0.0");
   SmearThetaHadronic.Accept.AddZone(AngleZoneHadronic);
   SmearThetaHadronic.Accept.SetGenre(Smear::kHadronic);    
   det.AddDevice(SmearThetaHadronic);
 
-  // XXX angular resolution?
-  Smear::Device SmearPhiHadronic(Smear::kPhi, "0.001");
+  Smear::Device SmearPhiHadronic(Smear::kPhi, "0.0");
   SmearPhiHadronic.Accept.AddZone(AngleZoneHadronic);
   SmearPhiHadronic.Accept.SetGenre(Smear::kHadronic);
   det.AddDevice(SmearPhiHadronic);
   
   // emcal stretches to -4.5 < eta < 4.5
-  // XXX angular resolution?
   Smear::Acceptance::Zone AngleZoneEmcal(ThetaFromEta ( 4.5 ),ThetaFromEta ( -4.5 ));
-  Smear::Device SmearThetaEmcal(Smear::kTheta, "0.001");
+  Smear::Device SmearThetaEmcal(Smear::kTheta, "0.0");
   SmearThetaEmcal.Accept.AddZone(AngleZoneEmcal);
   SmearThetaEmcal.Accept.SetGenre(Smear::kElectromagnetic);    
   det.AddDevice(SmearThetaEmcal);
   
-  // XXX angular resolution?
-  Smear::Device SmearPhiEmcal(Smear::kPhi, "0.001");
+  Smear::Device SmearPhiEmcal(Smear::kPhi, "0.0");
   SmearPhiEmcal.Accept.AddZone(AngleZoneEmcal);
   SmearPhiEmcal.Accept.SetGenre(Smear::kElectromagnetic);
   det.AddDevice(SmearPhiEmcal);
@@ -121,7 +106,6 @@ Smear::Detector BuildMatrixDetector_0_1() {
   Smear::Device TrackBack1P(Smear::kP, "sqrt( pow ( 0.001*P*P, 2) + pow ( 0.005*P, 2) )");
   TrackBack1P.Accept.AddZone(TrackBack1Zone);
   TrackBack1P.Accept.SetCharge(Smear::kCharged);
-  // TrackBack1P.Accept.SetGenre(Smear::kHadronic);
   det.AddDevice(TrackBack1P);
 
   // eta = -2.0 --  -1
@@ -130,7 +114,6 @@ Smear::Detector BuildMatrixDetector_0_1() {
   Smear::Device TrackBack2P(Smear::kP, "sqrt( pow ( 0.0005*P*P, 2) + pow ( 0.005*P, 2) )");
   TrackBack2P.Accept.AddZone(TrackBack2Zone);
   TrackBack2P.Accept.SetCharge(Smear::kCharged);
-  // TrackBack2P.Accept.SetGenre(Smear::kHadronic);
   det.AddDevice(TrackBack2P);
 
   // eta = -1 -- +1
@@ -139,7 +122,6 @@ Smear::Detector BuildMatrixDetector_0_1() {
   Smear::Device TrackBarrelP(Smear::kP, "sqrt( pow ( 0.0005*P*P, 2) + pow ( 0.005*P, 2) )");
   TrackBarrelP.Accept.AddZone(TrackBarrelZone);
   TrackBarrelP.Accept.SetCharge(Smear::kCharged);
-  // TrackBarrelP.Accept.SetGenre(Smear::kHadronic);
   det.AddDevice(TrackBarrelP);
 
   // eta = 1 -- 2.5
@@ -148,7 +130,6 @@ Smear::Detector BuildMatrixDetector_0_1() {
   Smear::Device TrackFwd2P(Smear::kP, "sqrt( pow ( 0.0005*P*P, 2) + pow ( 0.01*P, 2) )");
   TrackFwd2P.Accept.AddZone(TrackFwd2Zone);
   TrackFwd2P.Accept.SetCharge(Smear::kCharged);
-  // TrackFwd2P.Accept.SetGenre(Smear::kHadronic);
   det.AddDevice(TrackFwd2P);
 
   // eta = 2.5 -- 3.5
@@ -157,13 +138,7 @@ Smear::Detector BuildMatrixDetector_0_1() {
   Smear::Device TrackFwd1P(Smear::kP, "sqrt( pow ( 0.001*P*P, 2) + pow ( 0.02*P, 2) )");
   TrackFwd1P.Accept.AddZone(TrackFwd1Zone);
   TrackFwd1P.Accept.SetCharge(Smear::kCharged);
-  // TrackFwd1P.Accept.SetGenre(Smear::kHadronic);
   det.AddDevice(TrackFwd1P);
-
-  // TODO: Add low-Q^2 tagger: -6.9<eta<-5.8: Delta_theta/theta < 1.5%; 10^-6 < Q2 < 10^-2 GeV2
-  // TODO: Add proton spectrometer:  eta>6.2: sigma_intrinsic(|t|)/|t| < 1%; Acceptance: 0.2 < pT < 1.2 GeV/c
-  // TODO: Add material budget X/X0 <~5%
-  // TODO: Add vertexing: sigma_xyz ~ 20 microns, d0(z) ~ d0(r phi) ~ (20 microns)/(pT [GeV])  + 5 microns
   
   // EM Calorimeters
   // ---------------
@@ -172,17 +147,11 @@ Smear::Detector BuildMatrixDetector_0_1() {
   // Calorimeter resolution usually given as sigma_E/E = const% + stocastic%/Sqrt{E}
   // EIC Smear needs absolute sigma: sigma_E = Sqrt{const*const*E*E + stoc*stoc*E}
 
-  // IMPORTANT: The Handbook table 2 does not provide constant terms,
-  //            which may be important for some measurements.
-  //            We adapted the handbook here to values provided by the calo DWG where available
-  
   // Back
   // eta = -4.5 -- -2
   // stoch. = 2%
-  // Using const. = 1% (PANDA-ish) XXX
-  // Note: p. 41f calls for stoch = 1--1.5%, const <~ 0.5%
   Smear::Acceptance::Zone EmcalBackZone(ThetaFromEta ( -2 ),ThetaFromEta ( -4.5 ));
-  Smear::Device EmcalBack(Smear::kE, "sqrt( pow ( 0.01*E,2 ) + pow( 0.02,2)*E)");
+  Smear::Device EmcalBack(Smear::kE, "sqrt( pow ( 0.0*E,2 ) + pow( 0.02,2)*E)");
   EmcalBack.Accept.AddZone(EmcalBackZone);
   EmcalBack.Accept.SetGenre(Smear::kElectromagnetic);
   det.AddDevice(EmcalBack);
@@ -190,25 +159,21 @@ Smear::Detector BuildMatrixDetector_0_1() {
   // MidBack
   // eta = -2 -- -1
   // stoch. = 7%
-  // Could use const. = 1% (bit worse than KLOE) XXX
-  //   // but using the Calo group's alternative instead, Shashlyk, Pb/Sc 8% stochastic, 2% constant.XXX
   Smear::Acceptance::Zone EmcalMidBackZone(ThetaFromEta ( -1 ),ThetaFromEta ( -2 ));
-  Smear::Device EmcalMidBack(Smear::kE, "sqrt( pow ( 0.01*E,2 ) + pow( 0.07,2)*E)");
+  Smear::Device EmcalMidBack(Smear::kE, "sqrt( pow ( 0.0*E,2 ) + pow( 0.07,2)*E)");
   EmcalMidBack.Accept.AddZone(EmcalMidBackZone);
   EmcalMidBack.Accept.SetGenre(Smear::kElectromagnetic);
   det.AddDevice(EmcalMidBack);
   
   // Forward
   // eta = -1 -- 4.5
-  // stoch. = 10-12%.
-  // From calo group: using 12% and const.=2% (W/SciFi)
+  // stoch. = 10-12%, use 12%
   Smear::Acceptance::Zone EmcalFwdZone(ThetaFromEta ( 4.5 ),ThetaFromEta ( -1 ));
-  Smear::Device EmcalFwd(Smear::kE, "sqrt( pow ( 0.02*E,2 ) + pow( 0.12,2)*E)");
+  Smear::Device EmcalFwd(Smear::kE, "sqrt( pow ( 0.0*E,2 ) + pow( 0.12,2)*E)");
   EmcalFwd.Accept.AddZone(EmcalFwdZone);
   EmcalFwd.Accept.SetGenre(Smear::kElectromagnetic);
   det.AddDevice(EmcalFwd);
 
-  // TODO: Add PID
   // Could turn on perfect PID
   // Smear::Acceptance::Zone acceptall(etaToTheta(15.),etaToTheta(-15.));
   // Smear::PerfectID pid;
@@ -217,45 +182,45 @@ Smear::Detector BuildMatrixDetector_0_1() {
   
   // Hadronic  Calorimeters
   // ----------------------
-  // IMPORTANT: The Handbook table 2 does not provide constant terms,
-  //            which may be important for some measurements.
-  //            The barrel options modeled after existing instruments will have one,
-  //            leaving it at 0 otherwise
   // Note: kHadronic == |pdg|>110. 
     
   // Back
   // eta = -3.5 -- -1
   // stoch. = 50%
-  // Using configuration 2 from calo group, Pb/Sc 45% stochastic, 6% constant terms. XXX
   Smear::Acceptance::Zone HcalBackZone(ThetaFromEta ( -1 ),ThetaFromEta ( -3.5 ));
-  Smear::Device HcalBack(Smear::kE, "sqrt(pow( 0.00*E, 2) + pow ( 0.5,2) *E)");
+  Smear::Device HcalBack(Smear::kE, "sqrt(pow( 0.0*E, 2) + pow ( 0.5,2) *E)");
   HcalBack.Accept.AddZone(HcalBackZone);
   HcalBack.Accept.SetGenre(Smear::kHadronic);
   det.AddDevice(HcalBack);
   
   // Barrel
-  // Handbook: TBD XXX
-  // Providing two options
+  // eta = -1 -- 1
+  // The matrix has nothing. As examples, one could turn to
   // ~CMS
-  Smear::Device HcalBarrel(Smear::kE, "sqrt( pow( 0.07*E, 2)  + pow( 0.85, 2)*E)");
-  // Zeus
+  // Smear::Device HcalBarrel(Smear::kE, "sqrt( pow( 0.07*E, 2) + pow( 0.85, 2)*E)");
+  // ~Zeus
   // Smear::Device HcalBarrel(Smear::kE, "sqrt( pow( 0.02*E, 2) + pow( 0.35,2) *E)");
 
-  // eta = -1 -- 1
-  Smear::Acceptance::Zone HcalBarrelZone(ThetaFromEta ( 1 ),ThetaFromEta ( -1 ));
-  HcalBarrel.Accept.AddZone(HcalBarrelZone);
-  HcalBarrel.Accept.SetGenre(Smear::kHadronic);
-  det.AddDevice(HcalBarrel);
+  // Smear::Acceptance::Zone HcalBarrelZone(ThetaFromEta ( 1 ),ThetaFromEta ( -1 ));
+  // HcalBarrel.Accept.AddZone(HcalBarrelZone);
+  // HcalBarrel.Accept.SetGenre(Smear::kHadronic);
+  // det.AddDevice(HcalBarrel);
 
   // Forward
-  // eta = 1 -- 3.5 XXX
+  // eta = 1 -- 3.5
   // stoch. = 50%
-  // Using configuration 2 from calo group, Pb/Sc 45% stochastic, 6% constant terms.
   Smear::Acceptance::Zone HcalFwdZone(ThetaFromEta ( 3.5 ),ThetaFromEta ( 1 ));
-  Smear::Device HcalFwd(Smear::kE, "sqrt(pow( 0.00*E, 2) + pow ( 0.5,2) *E)");
+  Smear::Device HcalFwd(Smear::kE, "sqrt(pow( 0.0*E, 2) + pow ( 0.5,2) *E)");
   HcalFwd.Accept.AddZone(HcalFwdZone);
   HcalFwd.Accept.SetGenre(Smear::kHadronic);
   det.AddDevice(HcalFwd);
+
+  // Not covered:
+  // Tracker material budget X/X0 <~5%
+  // Low-Q^2 tagger: -6.9<eta<-5.8: Delta_theta/theta < 1.5%; 10^-6 < Q2 < 10^-2 GeV2
+  // Proton spectrometer:  eta>6.2: sigma_intrinsic(|t|)/|t| < 1%; Acceptance: 0.2 < pT < 1.2 GeV/c
+  // Barrel vertexing: sigma_xyz ~ 20 microns, d0(z) ~ d0(r phi) ~ (20 microns)/(pT [GeV])  + 5 microns
+  // Central muon detection
 
   return det;
 }
